9c98b91031583279c5b56bf0ed3113a17b41dbcf
Juan Carlos <92410948+JuanCarlosSn@users.noreply.github.com>
2024-12-10 17:44:02 -0600

    add icons (heroicons)

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { PlusIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from "../../Context";
 
 const Card = ({ data }) => {
@@ -16,7 +17,7 @@ const Card = ({ data }) => {
           alt={data.title}
         />
         <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1 pt-0" onClick={() => setCount(count + 1)}>
-          +
+          <PlusIcon className="h-6 w-6 text-black" />
         </div>
       </figure>
       <p className="flex justify-between">
