a8fdf03bdd4c5c95236e29a696d76bc1fea39d2a
Juan Carlos <92410948+JuanCarlosSn@users.noreply.github.com>
2024-12-17 17:33:18 -0600

    add total price to cart

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -3,7 +3,7 @@ import { useContext } from "react"
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from "../../Context"
 import OrderCard from "../OrderCard"
-
+import { totalPrice } from "../../Utils"
 import './styles.css'
 
 const CheckoutSideMenu = () => {
@@ -20,20 +20,26 @@ const CheckoutSideMenu = () => {
 				<h2 className='font-medium text-xl'>My Order</h2>
 				<div onClick={() => closeCheckoutSideMenu()}><XMarkIcon className="h-6 w-6 text-black cursor-pointer" /></div>
 			</div>
-				<div className="px-6 overflow-y-scroll">
-					{
-						cartProducts.map((product) => (
-							<OrderCard
-								key={product.id}
-                                id={product.id}
-								title={product.title}
-								imageUrl={product.images[0]}
-								price={product.price}
-                                handleDelete={() => handleDelete(product.id)}
-							/>
-						))
-					}
-				</div>
+			<div className="px-6 overflow-y-scroll">
+				{
+					cartProducts.map((product) => (
+						<OrderCard
+							key={product.id}
+							id={product.id}
+							title={product.title}
+							imageUrl={product.images[0]}
+							price={product.price}
+							handleDelete={() => handleDelete(product.id)}
+						/>
+					))
+				}
+			</div>
+			<div className="px-6">
+				<p className="flex justify-between items-center ">
+					<span className="font-light">Total:</span>
+					<span className="font-medium text-2xl">${totalPrice(cartProducts)}</span>
+				</p>
+			</div>
 		</aside>
 	);
 };
---
+++ src/Utils/index.js
@@ -0,0 +1,10 @@
+/**
+ * This funtion calculates the total price of a new order
+ * @param {Array} products cartProducts: Array of Objects
+ * @returns {Number} total price
+ */
+export const totalPrice = (products) => {
+    let sum = 0
+    products.forEach(product => sum += product.price)
+    return sum    
+}
