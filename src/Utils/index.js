44072252a4b4f857ab90b044e975398485307750
Juan Carlos <92410948+JuanCarlosSn@users.noreply.github.com>
2024-12-20 18:08:51 -0600

    feat: Agregar key de account y sign-out en localStorage

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,7 +1,7 @@
 /**
  * This function calculates total price of a new order
  * @param {Array} products cartProduct: Array of Objects
- * @returns {Number} total price
+ * @returns {numer} Total price
  */
 export const totalPrice = (products) => {
   let sum = 0
